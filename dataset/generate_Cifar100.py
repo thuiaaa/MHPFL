dd00016bc90eebbbbfe452ad3c7904f15cd18481
Liang_personal <tonghuiaaa@outlook.com>
2025-03-02 14:39:22 +0800

    modified generate data

---
+++ dataset/generate_Cifar100.py
@@ -0,0 +1,100 @@
+# MHPFLlib: Model Heterogeneous Personalized Federated Learning Algorithm Library
+# Copyright (C) 2025  Tonghui Liang
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import numpy as np
+import os
+import argparse
+import random
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from utils.dataset_utils import check, separate_data, split_data, save_file
+
+
+# Allocate data to users
+def generate_dataset(dir_path, num_clients, niid, balance, partition):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    # Setup directory for train/test data
+    config_path = dir_path + "config.json"
+    train_path = dir_path + "train/"
+    test_path = dir_path + "test/"
+
+    if check(config_path, train_path, test_path, num_clients, niid, balance, partition):
+        return
+
+    # Get Cifar100 data
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR100(
+        root=dir_path+"rawdata", train=True, download=True, transform=transform)
+    testset = torchvision.datasets.CIFAR100(
+        root=dir_path+"rawdata", train=False, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=len(trainset.data), shuffle=False)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=len(testset.data), shuffle=False)
+
+    for _, train_data in enumerate(trainloader, 0):
+        trainset.data, trainset.targets = train_data
+    for _, test_data in enumerate(testloader, 0):
+        testset.data, testset.targets = test_data
+
+    dataset_image = []
+    dataset_label = []
+
+    dataset_image.extend(trainset.data.cpu().detach().numpy())
+    dataset_image.extend(testset.data.cpu().detach().numpy())
+    dataset_label.extend(trainset.targets.cpu().detach().numpy())
+    dataset_label.extend(testset.targets.cpu().detach().numpy())
+    dataset_image = np.array(dataset_image)
+    dataset_label = np.array(dataset_label)
+
+    num_classes = len(set(dataset_label))
+    print(f'Number of classes: {num_classes}')
+
+    # dataset = []
+    # for i in range(num_classes):
+    #     idx = dataset_label == i
+    #     dataset.append(dataset_image[idx])
+
+    X, y, statistic = separate_data((dataset_image, dataset_label), num_clients, num_classes,
+                                    niid, balance, partition, class_per_client=2)
+    train_data, test_data = split_data(X, y)
+    save_file(config_path, train_path, test_path, train_data, test_data, num_clients, num_classes,
+        statistic, niid, balance, partition)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='generate data')
+    parser.add_argument('--seed', type=int, default=1)
+    parser.add_argument('--dir_path', type=str, default="Cifar100/", help="directory path to save data")
+    parser.add_argument('--noniid', type=str, default="True", help="Non-IID for True")
+    parser.add_argument('--balance', type=str, default="True", help="Balance for True")
+    parser.add_argument('--partition', type=str, default="pat", help="pat(pathological) or dir(practical) or nothing ")
+    parser.add_argument('--num_clients', type=int, default=10, help="number of clients")
+
+    args = parser.parse_args()
+
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    args.noniid = True if args.noniid == "True" else False
+    args.balance = True if args.balance == "True" else False
+
+    generate_dataset(args.dir_path, args.num_clients, args.noniid, args.balance, args.partition)
