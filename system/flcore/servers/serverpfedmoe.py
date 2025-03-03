29a9dad2d3573f203be26a5762a100d02817e042
Liang_personal <tonghuiaaa@outlook.com>
2025-03-03 13:06:17 +0800

    update

--- system/flcore/servers/serverpfedmoe.py
+++ system/flcore/servers/serverpfedmoe.py
@@ -9,3 +9,21 @@ from flcore.clients.clientpfedmoe import clientpfedmoe
 from utils.data_utils import read_client_data
 from threading import Thread
 from collections import defaultdict
+
+
+class pFedMoE(Server):
+    def __init__(self, args, times):
+        super().__init__()
+
+        self.set_slow_clients()
+        self.set_clients(clientpfedmoe)
+
+        print(f"\nJoin ratio / total clients: {self.join_ratio} / {self.num_clients}")
+        print("Finished creating server and clients.")
+
+        self.Budget = []
+        self.num_classes = args.num_classes
+
+    def train(self):
+
+
